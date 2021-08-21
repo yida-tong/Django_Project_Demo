626c7804ad2edc2d7e54e29862d018518776564c
yida <tong.yida88@gmail.com>
2021-08-21 10:58:13 -0700

    construct shipment data import and modify shipment model

--- data_warehouse/views.py
+++ data_warehouse/views.py
@@ -3,6 +3,7 @@ import csv
 import requests
 import datetime
 import decimal
+import random
 from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse, HttpResponseNotFound, HttpResponseBadRequest
 from django.contrib import messages
@@ -10,7 +11,8 @@ from .models import *
 
 
 def index(request):
-    context_dict = {'payers_num': Payer.objects.count()
+    context_dict = {'payers_num': Payer.objects.count(),
+                    'step1': True
                     }
     return render(request, 'data_warehouse/dashboard.html', context_dict)
 
@@ -18,10 +20,10 @@ def index(request):
 def csv_downloader(request):
     if request.method == 'GET' and len(request.GET) > 0:
         if request.GET['action'] == 'payers':
-            url = 'https://www.dropbox.com/s/eo5s6owm5saqa25/payer%20%281%29.csv?raw=1'
+            url = 'https://www.dropbox.com/s/ossuev7cylglm8n/payer.csv?raw=1'
             file_name = 'payers.csv'
         elif request.GET['action'] == 'shipments':
-            url = 'https://www.dropbox.com/s/7q4x19ueer3ubyd/payer.csv?raw=1'
+            url = 'https://www.dropbox.com/s/0tfzh78v5dady1l/shipments.csv?raw=1'
             file_name = 'shipments.csv'
         else:
             return HttpResponseBadRequest()
@@ -37,15 +39,16 @@ def csv_downloader(request):
 
 
 def csv_uploader(request):
+    context_dict = {'step2': True}
     if request.method == 'POST' and request.POST['action'] == 'upload':
         payer_file = request.FILES['payers_csv']
-        # shipment_file = request.FILES['shipment_csv']
-        if not payer_file.name.endswith('.csv'):
+        shipment_file = request.FILES['shipment_csv']
+        if not payer_file.name.endswith('.csv') or not shipment_file.name.endswith('.csv'):
             messages.error(request, 'Not a csv file')
-            return render(request, 'data_warehouse/dashboard.html', {})
-        if payer_file.multiple_chunks():
-            messages.error(request, 'File size too big (%.2f MB)' % (payer_file.size / (1000 * 1000)))
-            return render(request, 'data_warehouse/dashboard.html', {})
+            return render(request, 'data_warehouse/dashboard.html', context_dict)
+        if payer_file.multiple_chunks() or shipment_file.multiple_chunks():
+            messages.error(request, 'File size too big (%.2f MB, %.2f MB)' % (payer_file.size / (1000 * 1000), shipment_file.size / (1000 * 1000)))
+            return render(request, 'data_warehouse/dashboard.html', context_dict)
 
         text = io.StringIO(payer_file.read().decode('utf-8'))
         bulk_list = []
@@ -65,19 +68,48 @@ def csv_uploader(request):
                 bulk_list.append(Payer(**bulk_dict))
         if bulk_list:
             Payer.objects.bulk_create(bulk_list)
-            messages.success(request, 'Data is added')
+            pks = list(Payer.objects.all())
+            text1 = io.StringIO(shipment_file.read().decode('utf-8'))
+            bulk_list1 = []
+            for line in csv.reader(text1):
+                if line:
+                    bulk_dict1 = {
+                        'TrackingNum': line[0],
+                        'ShipDate': datetime.datetime.strptime(line[1], '%Y-%m-%d').date(),
+                        'status': line[2],
+                        'delivery_date': datetime.datetime.strptime(line[3], '%Y-%m-%d').date() if line[3] else None,
+                        'service_type': line[4],
+                        'PackageWeight': decimal.Decimal(line[5]),
+                        'ShipperStreet': line[6],
+                        'ShipperCity': line[7],
+                        'ShipperState': line[8],
+                        'ShipperZip': line[9],
+                        'RecipientStreet': line[10],
+                        'RecipientCity': line[11],
+                        'RecipientState': line[12],
+                        'RecipientZip': line[13],
+                        'payer': random.choice(pks)
+                    }
+                    bulk_list1.append(Shipment(**bulk_dict1))
+            if bulk_list1:
+                Shipment.objects.bulk_create(bulk_list1)
+                messages.success(request, 'Data is added')
+            else:
+                messages.warning(request, 'No data is read')
         else:
             messages.warning(request, 'No data is read')
-        return render(request, 'data_warehouse/dashboard.html')
+        return render(request, 'data_warehouse/dashboard.html', context_dict)
     else:
         return HttpResponseBadRequest()
 
 
 def reset(request):
+    context_dict = {'reset': True}
     if request.method == 'POST' and request.POST['action'] == 'reset':
         Payer.objects.all().delete()
+        Shipment.objects.all().delete()
         messages.success(request, 'Reset done')
-        return render(request, 'data_warehouse/dashboard.html')
+        return render(request, 'data_warehouse/dashboard.html', context_dict)
     else:
         return HttpResponseBadRequest()
 
