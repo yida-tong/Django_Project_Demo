7dac14a8fe5d3cd922573d67f7fbaf22963c483e
yida <tong.yida88@gmail.com>
2022-06-12 06:41:49 -0700

    test

--- data_warehouse/views.py
+++ data_warehouse/views.py
@@ -378,13 +378,13 @@ def aljazeera_sentiment_analysis(request):
             response['wordsFreq_axis'].append(v)
 
         # generate wordcloud img
-        # text = ' '.join([i for i in response['words_axis']])
-        # wordcloud = WordCloud(
-        #     background_color='black',
-        #     max_words=25,
-        #     width=1400,
-        #     height=1200
-        # ).generate(text)
+        text = ' '.join([i for i in response['words_axis']])
+        wordcloud = WordCloud(
+            background_color='black',
+            max_words=25,
+            width=1400,
+            height=1200
+        ).generate(text)
         # wordcloud.to_file("./static/img/worldcloud/wc_{}.png".format(requestIndex))
     else:
         return HttpResponseBadRequest()
