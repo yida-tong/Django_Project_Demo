eb3686e0eaa2a58cae30959f27eacbec20c84632
yida <tong.yida88@gmail.com>
2022-06-12 17:05:58 -0700

    final fix

--- django_qualified/urls.py
+++ django_qualified/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('mysite/', include('mysite.urls', namespace='mysite')),
     path('data_warehouse/', include('data_warehouse.urls', namespace='data_warehouse')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
